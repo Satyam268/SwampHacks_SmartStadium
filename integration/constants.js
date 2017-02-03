9344e6ed6662a9422da96b0f9dcc057129fa73be
Satyam <satyam268@gmail.com>
2017-02-03 12:42:55 -0500

    updated
    updated constant

--- integration/constants.js
+++ integration/constants.js
@@ -4,4 +4,4 @@
 /**
  * Created by mypc on 1/19/2017.
  */
-module.exports.AccessToken = "EAACEdEose0cBADP8SDWI7KZAfEPkIPClqI1osSQ9y03trgy5nPqlTzOq7jPfwGLuOmoZCQPsvqky9NOSPZCfwEGgiNTcLbQd2f495Osg5ZCueMZC7GZBrFEMRP2p3zg2S6wjZBzCBZAiiAy7phAW6alg6DiiC5VdpeQrKsh4UkEiPxPtdcT6BbuygU1KRWHsmnIZD";
+module.exports.AccessToken = "xxxxx";
